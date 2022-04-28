02cfaf745eec6bce8a82428223e0d10f0491fed1
Daniel Werner <99842410+DanielWerner07@users.noreply.github.com>
2022-04-28 18:43:56 -0500

    added more test

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,7 @@
 package test;
 
+import jdk.jfr.Name;
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,9 +10,63 @@ public class BalancedBracketsTest {
 
     //TODO: add tests here
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
     }
 
+    @Test
+    public  void onlyBracketsReturnsFalse() {
+        assertFalse((BalancedBrackets.hasBalancedBrackets("][]")));
+    }
+
+    @Test
+    public void misplacedBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void noBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("LaunchCode"));
+    }
+
+    @Test
+    public void nothingGiven() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void largeAmountOfBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[][][][][]"));
+    }
+
+    @Test
+    public void test7() {
+
+    }
+
+    @Test
+    public void test8() {
+
+    }
+
+    @Test
+    public void test9() {
+
+    }
+
+    @Test
+    public void test10() {
+
+    }
+
+    @Test
+    public void test11() {
+
+    }
+
+    @Test
+    public void test12() {
+
+    }
 
 }
